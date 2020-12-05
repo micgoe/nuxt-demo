ccf05d125577b19df4f52a412cc3df0d9b8656ed
michaelgoehrig <michaelgoehrig@web.de>
2020-12-05 12:54:56 +0100

    Init

---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <header>
+      <nav>
+        <nuxt-link to="/">Home</nuxt-link>
+        <nuxt-link to="/about-us">About us</nuxt-link>
+      </nav>
+    </header>
+    <div class="container mx-auto">
+      <h1>Home</h1>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style>
+/* .container {
+  @apply min-h-screen flex justify-center items-center text-center mx-auto;
+} */
+</style>
